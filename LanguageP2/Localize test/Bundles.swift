fcc2cf8edbba8505cc73582487966d19c3f37afd
ynnenu <enielav1997@hotmail.com>
2021-02-21 21:40:54 -0500

    Add files via upload

---
+++ LanguageP2/Localize test/Bundles.swift
@@ -0,0 +1,38 @@
+//
+//  Bundles.swift
+//  Localize test
+//
+//  Created by Eniela Vela on 10.12.20.
+//
+
+import Foundation
+import UIKit
+
+extension Bundle {
+    private static var bundle: Bundle!
+
+    public static func localizedBundle() -> Bundle! {
+        if bundle == nil {
+            let appLang = UserDefaults.standard.string(forKey: "app_lang") ?? "ru"
+            let path = Bundle.main.path(forResource: appLang, ofType: "lproj")
+            bundle = Bundle(path: path!)
+        }
+
+        return bundle;
+    }
+
+    public static func setLanguage(lang: String) {
+        UserDefaults.standard.set(lang, forKey: "app_lang")
+        let path = Bundle.main.path(forResource: lang, ofType: "lproj")
+        bundle = Bundle(path: path!)
+    }
+}
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.localizedBundle(), value: "", comment: "")
+    }
+
+    func localizeWithFormat(arguments: CVarArg...) -> String{
+        return String(format: self.localized(), arguments: arguments)
+    }
+}
